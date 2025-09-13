5c7975a2431111b82910b4815beacc0adb9609a9
tushar6707-Aenaen <chauhantushar6706@gmail.com>
2025-09-13 23:05:58 +0530

    Fix: Update repository structure and configurations

--- nasa-react-app/src/App.jsx
+++ nasa-react-app/src/App.jsx
@@ -2,12 +2,16 @@
 import Main from './components/Main'
 import Sidebar from './components/Sidebar'
 import Footer from './components/Footer'
+import { useState } from 'react'
 
 const App = () => {
+  const [showModel,setShowModel] = useState(false);
+
+
   return (
     <>
         <Main/>
-        <Sidebar/>
+        {showModel && (<Sidebar/>) }
         <Footer/>
     </>
   )
--- nasa-react-app/src/components/Sidebar.jsx
+++ nasa-react-app/src/components/Sidebar.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 
 const Sidebar = () => {
-  return (
-    <aside>
-        <h2>The Brutal Martian Landscape</h2>
-      <div>
+    return (
+        <aside className='sidebar'>
+            <div className='bg-overlay'> </div>
+            <div className='sidebar-contents'>
 
-        <p>Description</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, enim laborum eius pariatur quaerat temporibus itaque aperiam expedita, ipsa repellat quas alias illum quisquam rerum nesciunt praesentium perspiciatis numquam! Dolorem!</p>
-      </div>
-    </aside>
-  )
+                <h2>The Brutal Martian Landscape</h2>
+                <div>
+
+                    <p>Description</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, enim laborum eius pariatur quaerat temporibus itaque aperiam expedita, ipsa repellat quas alias illum quisquam rerum nesciunt praesentium perspiciatis numquam! Dolorem!</p>
+                </div>
+                <button><i className="fa-solid fa-arrow-right"></i></button>
+            </div>
+        </aside>
+    )
 }
 
 export default Sidebar
